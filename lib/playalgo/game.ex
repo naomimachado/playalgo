91c684a8a4991546f78ac21bf08e45b53768f6bf
purnesh42h <purnesh.dixit92@gmail.com>
2018-03-19 04:38:09 +0000

    Game channel join done

--- lib/playalgo/game.ex
+++ lib/playalgo/game.ex
@@ -1,25 +1,52 @@
 defmodule Playalgo.Game do
   def new do
     %{
-      guesses: %{
-        player: "test",
-        list: []
+      player1: %{
+        name: "",
+        score: 0,
+        guess_list: [],
+        challenge: 0
+      },
+      player2: %{
+        name: "",
+        score: 0,
+        guess_list: [],
+        challenge: 0
       }
     }
+  end
+
+  defp skeleton(player, target) do
+    %{
+      name: player[:name],
+      target: target,
+      guess_list: player[:guess_list]
+    }
+  end
+  
+  defp init_player(player, player_name, challenge) do
+    Map.put(player, :name, player_name)
+      |> Map.put(:challenge, elem(Integer.parse(challenge), 0))
   end
   
   def client_view(game) do
     %{
-       guesses: game.guesses
+      player1_skeleton: skeleton(game.player1, game.player2[:challenge]),
+      player2_skeleton: skeleton(game.player2, game.player1[:challenge])
     }
   end
 
-  def guess(game, id, active) do
-    guesses = %{
-      player: "test",
-      list: []
-    }
-    Map.put(game, :guesses, guesses)
+  def guess(game, number) do
+    Map.put(game, :player1, game.player1)
+    |> Map.put(:player2, game.player2)
+  end
+
+  def challenge(game, player_name, challenge) do
+    if game.player1.name == "" do
+      Map.put(game, :player1, init_player(game.player1, player_name, challenge))
+    else
+      Map.put(game, :player2, init_player(game.player2, player_name, challenge))
+    end
   end
 end
 
--- lib/playalgo_web/channels/games_channel.ex
+++ lib/playalgo_web/channels/games_channel.ex
@@ -5,7 +5,7 @@ defmodule PlayalgoWeb.GamesChannel do
 
   def join("games:" <> name, payload, socket) do
     if authorized?(payload) do
-      IO.puts "hi\n"
+      Playalgo.GameBackup.load(name)
       game = Playalgo.GameBackup.load(name) || Game.new()
       socket = socket
       |> assign(:game, game)
@@ -18,13 +18,23 @@ defmodule PlayalgoWeb.GamesChannel do
 
   # Channels can be used in a request/response fashion
   # by sending replies to requests from the client
-  def handle_in("guess", %{"id" => ll, "active" => active}, socket) do
-    game = Game.guess(socket.assigns[:game], ll, active)
+  def handle_in("guess", %{"number" => guess}, socket) do
+    game = Game.guess(socket.assigns[:game], guess)
     Playalgo.GameBackup.save(socket.assigns[:name], game)
     socket = assign(socket, :game, game)
     {:reply, {:ok, %{ "game" => Game.client_view(game)}}, socket}
   end
-
+  
+  def handle_in("challenge", %{"player_name" => player_name, "challenge" => challenge}, socket) do
+    game = Game.challenge(socket.assigns[:game], player_name, challenge)
+    Playalgo.GameBackup.save(socket.assigns[:name], game)
+    socket = assign(socket, :game, game)
+    {:reply, {:ok, %{ "game" => Game.client_view(game)}}, socket}
+  end
+  
+  def handle_in("games", %{}, socket) do
+    {:reply, {:ok, %{ "games" => Playalgo.GameBackup.games()}}, socket}
+  end
   # Add authorization logic here as required.
   defp authorized?(_payload) do
     true
