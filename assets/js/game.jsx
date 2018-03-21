750a85054ee6d01bff90062d58289ee919e5ddd3
naomimachado21 <naomimachado21@gmail.com>
2018-03-21 16:43:10 -0400

    clickable nos done, elixir side still todo

--- assets/js/game.jsx
+++ assets/js/game.jsx
@@ -5,12 +5,14 @@ export default function game_init(root, channel) {
   ReactDOM.render(<Game channel={channel} />, root);
 }
 
+
 class Game extends React.Component{
   constructor(props)  {
     super(props);
     this.channel = props.channel;
     this.state = {
-      listData: trying()
+      listData: trying(),
+      clickedList: clickedNosList(trying())
     };
     console.log(this.state.listData);
     this.channel.join()
@@ -28,10 +30,6 @@ class Game extends React.Component{
     console.log(this.state);
   }
 
-  selected(num){
-    this.channel.push("guessed_number",  {guess: num})
-      .receive("ok", this.gotView.bind(this))
-  }
 
   render() {
     let game_list = _.map(this.state.game_list, (game, ii) => {
@@ -40,6 +38,10 @@ class Game extends React.Component{
     let nums = _.map(this.state.listData, (num, ii) => {
       return <RenderList num={num} />;
     });
+    let clickednums = _.map(this.state.listData, (cnum, ii) => {
+      return <RenderClickedList cnum={cnum} />;
+    });
+    console.log(this.state);
     return (
       <div className="rows">
         <div className="cols">
@@ -48,6 +50,10 @@ class Game extends React.Component{
         </div>
         <div className="cols">
           {nums}
+        </div><br></br>
+        <div className="cols">
+          Clicked Numbers:
+          {clickednums}
         </div>
       </div>
     )
@@ -58,10 +64,22 @@ function trying() {
   var lst = [];
   for(var i = 0; i < 20; i++)
   {
-    lst[i] = {value: i};
+    lst[i] = {value: i, clicked: false};
   }
   console.log(lst);
   return lst;
+}
+
+function clickedNosList(listData) {
+  var clst = [];
+  for(var i = 0; i < 20; i++)
+  {
+    if(listData[i].clicked == true)
+    {
+      clst = add(listData[i]);
+    }
+  }
+  return clst;
 }
 
 function RenderList(props) {
@@ -71,13 +89,35 @@ function RenderList(props) {
 
   return (
     <span className="rows">
-      <span id="num">
+      <span id="num" onClick={()=> clickedNum(listData)}>
         {num}
       </span>
     </span>
   )
 }
 
+function RenderClickedList(props) {
+  let clicked = props.cnum;
+  let show = "";
+  console.log("clicked",props.cnum);
+  let cnum = clicked.value;
+  if(cnum.clicked == true){
+    show = cnum.value;
+  }
+    return (
+      <span className="rows">
+        <span id="num">
+          {show}
+        </span>
+      </span>
+    )
+}
+
+function clickedNum(listData) {
+  let num = listData;
+  num.clicked = true;
+  console.log(num);
+}
 
 
 function GuessOpponentGame(params) {
